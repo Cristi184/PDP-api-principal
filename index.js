93d01fdf52730b5cb9b745819255fdb0a6bfb45d
cristian.stan1 <cristian.stan@ivfuture.uk>
2022-10-26 10:30:06 +0300

    add limit in mega

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ mongoose.connection.on('disconected', () => {
 //middlewares
 app.use(cors())
 app.use(cookieParser())
-app.use(express.json())
+app.use(express.json({limit: '50mb'}))
 
 app.use("/api/auth", authRoute)
 app.use("/api/sections", sectionsRoute)
